cd4e580393a3bcff14e2f12ec5af0bf819c9b56c
Zack Tanner <zacktanner@gmail.com>
2023-11-17 12:57:23 -0800

    disable static prefetches

--- test/e2e/app-dir/app-prefetch/prefetching.test.ts
+++ test/e2e/app-dir/app-prefetch/prefetching.test.ts
@@ -301,24 +301,76 @@ createNextDescribe(
       expect(prefetchResponse).toContain('Loading Prefetch Auto')
     })
 
-    it('should not generate static prefetches for layouts that opt into dynamic rendering', async () => {
-      await next.stop()
-      const rootLoading = await next.readFile('./app/loading.js')
-      await next.deleteFile('./app/loading.js')
-      await next.start()
-      expect(
-        await next
-          .readFile('.next/server/app/prefetch-dynamic-usage/foo.prefetch.rsc')
+    describe('dynamic rendering', () => {
+      async function hasPrefetch(filePath: string) {
+        return await next
+          .readFile(`.next/server/app${filePath}`)
           .catch(() => false)
-      ).toBeFalsy()
+      }
+      it('should not generate a static prefetch for layouts that use cookies/headers', async () => {
+        expect(
+          await hasPrefetch('/prefetch-dynamic-usage/foo.prefetch.rsc')
+        ).toBeFalsy()
 
-      expect(
-        await next
-          .readFile('.next/server/app/prefetch-dynamic-usage/foo.prefetch.rsc')
-          .catch(() => false)
-      ).toBeFalsy()
+        expect(
+          await hasPrefetch('/prefetch-dynamic-usage/bar.prefetch.rsc')
+        ).toBeFalsy()
+      })
 
-      await next.patchFile('./app/loading', rootLoading)
+      describe.each(['/force-dynamic', '/revalidate-0'])('%s', (basePath) => {
+        it('should not re-render layout when navigating between sub-pages', async () => {
+          const logStartIndex = next.cliOutput.length
+
+          const browser = await next.browser(`${basePath}/test-page`)
+          let initialRandomNumber = await browser
+            .elementById('random-number')
+            .text()
+          await browser
+            .elementByCss(`[href="${basePath}/test-page/sub-page"]`)
+            .click()
+
+          await check(() => browser.hasElementByCssSelector('#sub-page'), true)
+
+          const newRandomNumber = await browser
+            .elementById('random-number')
+            .text()
+
+          expect(initialRandomNumber).toBe(newRandomNumber)
+
+          await check(() => {
+            const logOccurrences =
+              next.cliOutput.slice(logStartIndex).split('re-fetching in layout')
+                .length - 1
+
+            return logOccurrences
+          }, 1)
+        })
+
+        it('should update search params following a link click', async () => {
+          const browser = await next.browser(`${basePath}/search-params`)
+          await check(
+            () => browser.elementById('search-params-data').text(),
+            /{}/
+          )
+          await browser.elementByCss('[href="?foo=true"]').click()
+          await check(
+            () => browser.elementById('search-params-data').text(),
+            /{"foo":"true"}/
+          )
+          await browser
+            .elementByCss(`[href="${basePath}/search-params"]`)
+            .click()
+          await check(
+            () => browser.elementById('search-params-data').text(),
+            /{}/
+          )
+          await browser.elementByCss('[href="?foo=true"]').click()
+          await check(
+            () => browser.elementById('search-params-data').text(),
+            /{"foo":"true"}/
+          )
+        })
+      })
     })
   }
 )
--- test/e2e/app-dir/app/index.test.ts
+++ test/e2e/app-dir/app/index.test.ts
@@ -37,7 +37,8 @@ createNextDescribe(
         )
       })
 
-      it('should use RSC prefetch data from build', async () => {
+      // TODO: Re-enable once static prefetches are re-landed
+      it.skip('should use RSC prefetch data from build', async () => {
         expect(
           await next.readFile('.next/server/app/linking.prefetch.rsc')
         ).toBeTruthy()
